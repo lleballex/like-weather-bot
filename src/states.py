5e81d7f99ff7dc9af72ea2908b1f0110dad9eec1
lleballex <lleballex@yandex.ru>
2022-04-24 16:59:29 +0300

    Removed commented out code

--- src/states.py
+++ src/states.py
@@ -3,10 +3,3 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class CityChanging(StatesGroup):
     title_wait = State()
-
-# class CityAdding(StatesGroup):
-#     title_wait = State()
-
-
-# class CitiesView(StatesGroup):
-#     action_wait = State()
